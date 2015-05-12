8861c405190c669099419853c2fe8a254bf737e5
Jakub Narloch <jmnarloch@gmail.com>
2015-05-12 23:26:52 +0200

    MinimumSizeSubarraySum.java

---
+++ src/main/java/MinimumSizeSubarraySum.java
@@ -0,0 +1,49 @@
+/**
+ *
+ */
+public class MinimumSizeSubarraySum {
+
+    public int minSubArrayLen(int s, int[] nums) {
+
+        // input: the search number, the array of integers
+        // output: the smallest subarray
+        // edge cases: nums is null, nums is empty
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int lo = 0;
+        int hi = 0;
+        int sum = 0;
+
+        int min = Integer.MAX_VALUE;
+        while (hi < nums.length) {
+
+            sum += nums[hi];
+            if (sum >= s) {
+                min = Math.min(min, hi - lo + 1);
+
+                int ind = lo;
+                while (ind <= hi && sum - nums[ind] >= s) {
+                    sum -= nums[ind];
+                    ind++;
+                }
+                min = Math.min(min, hi - ind + 1);
+                lo = ind;
+            }
+            hi++;
+        }
+
+        return min != Integer.MAX_VALUE ? min : 0;
+    }
+
+    public static void main(String[] args) {
+
+        final int s = 7;
+        final int[] nums = new int[]{2, 3, 1, 2, 4, 3};
+
+        final MinimumSizeSubarraySum min = new MinimumSizeSubarraySum();
+        System.out.println(min.minSubArrayLen(s, nums));
+    }
+}
