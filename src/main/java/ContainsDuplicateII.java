ae1901b94a3b009e953c60e9806cd1e7793e40fb
Jakub Narloch <jmnarloch@gmail.com>
2015-05-30 14:41:22 +0200

    ContainsDuplicateII.java

---
+++ src/main/java/ContainsDuplicateII.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ */
+public class ContainsDuplicateII {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return false;
+        }
+
+        final Map<Integer, Integer> occurences = new HashMap<>();
+        for (int ind = 0; ind < nums.length; ind++) {
+            if (occurences.containsKey(nums[ind])) {
+                final int prev = occurences.get(nums[ind]);
+                if (ind - prev <= k) {
+                    return true;
+                }
+            }
+            occurences.put(nums[ind], ind);
+        }
+        return false;
+    }
+}
