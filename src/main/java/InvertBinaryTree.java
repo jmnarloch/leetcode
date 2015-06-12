ff03f3a6030636d6045dca407376d28d5bf3caa6
Jakub Narloch <jmnarloch@gmail.com>
2015-06-12 22:15:47 +0200

    InvertBinaryTree.java

---
+++ src/main/java/InvertBinaryTree.java
@@ -0,0 +1,82 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ */
+public class InvertBinaryTree {
+
+    public TreeNode invertTree(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        final TreeNode left = root.left,
+                right = root.right;
+        root.left = invertTree(right);
+        root.right = invertTree(left);
+        return root;
+    }
+
+    public TreeNode invertTree2(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        final Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            final TreeNode node = stack.pop();
+            final TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
+
+    public TreeNode invertTree3(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        final Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            final TreeNode node = queue.poll();
+            final TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
