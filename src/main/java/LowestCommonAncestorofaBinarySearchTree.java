193299bc5efc624cafd93a573eb003f878057bf7
Jakub Narloch <jmnarloch@gmail.com>
2015-07-13 10:36:24 +0200

    Alternative implementation

--- src/main/java/LowestCommonAncestorofaBinarySearchTree.java
+++ src/main/java/LowestCommonAncestorofaBinarySearchTree.java
@@ -3,9 +3,26 @@
  */
 public class LowestCommonAncestorofaBinarySearchTree {
 
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if(root == null || p == null || q == null) {
+            return null;
+        }
+
+        if(root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+
+        if(root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+
+        return root;
+    }
+
     private TreeNode lca;
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
 
         if(root == null || p == null || q == null) {
             return null;
